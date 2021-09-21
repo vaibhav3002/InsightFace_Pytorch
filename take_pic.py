9d67ab31f5498e20dc49b2e8594963d556adbfa3
Gregory Lemercier <gregory.lemercier@leroymerlin.fr>
2021-09-21 16:08:24 +0200

    rework 'take_pic' to use single input image instead of video

--- take_pic.py
+++ take_pic.py
@@ -19,40 +19,13 @@ save_path = data_path/'facebank'/args.name
 if not save_path.exists():
     save_path.mkdir()
 
-# 初始化摄像头
-cap = cv2.VideoCapture(args.image)
-# 我的摄像头默认像素640*480，可以根据摄像头素质调整分辨率
-cap.set(3,1280)
-cap.set(4,720)
 mtcnn = MTCNN()
 
-while cap.isOpened():
-    # 采集一帧一帧的图像数据
-    isSuccess,frame = cap.read()
-    # 实时的将采集到的数据显示到界面上
-    if args.image == 0 and isSuccess:
-        frame_text = cv2.putText(frame,
-                    'Press t to take a picture,q to quit.....',
-                    (10,100), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 
-                    2,
-                    (0,255,0),
-                    3,
-                    cv2.LINE_AA)
-        cv2.imshow("My Capture",frame_text)
-    # 实现按下“t”键拍照
-    if not args.image == 0 or cv2.waitKey(1)&0xFF == ord('t'):
-        p =  Image.fromarray(frame[...,::-1])
-        try:            
-            warped_face = np.array(mtcnn.align(p))[...,::-1]
-            cv2.imwrite(str(save_path/'{}.jpg'.format(str(datetime.now())[:-7].replace(":","-").replace(" ","-"))), warped_face)
-            print('image saved')
-        except:
-            print('no face captured')
-        
-    if not args.image == 0 or cv2.waitKey(1)&0xFF == ord('q'):
-        break
-
-# 释放摄像头资源
-cap.release()
-#cv2.destoryAllWindows()
+try:
+    frame = cv2.imread(args.image)
+    p =  Image.fromarray(frame[...,::-1])
+    warped_face = np.array(mtcnn.align(p))[...,::-1]
+    cv2.imwrite(str(save_path/'{}.jpg'.format(str(datetime.now())[:-7].replace(":","-").replace(" ","-"))), warped_face)
+    print('image saved')
+except:
+    print('no face captured')
